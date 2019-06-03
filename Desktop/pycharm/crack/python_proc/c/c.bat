1fd5999eb2831f17e2ab97fc2fc89247b87f378d
piglaker <39005549+piglaker@users.noreply.github.com>
2019-06-03 10:01:12 +0800

    c++ zhang suen skeleton algorithm

---
+++ Desktop/pycharm/crack/python_proc/c/c.bat
@@ -0,0 +1,2 @@
+c.exe C:\\Users\\zhang\\Desktop\\pycharm\\crack\\python_proc\\deeplab\\1.png C:\\Users\\zhang\\Desktop\\pycharm\\crack\\python_proc\\concat
+pause
---
+++ Desktop/pycharm/crack/python_proc/c/readme.txt
@@ -0,0 +1,2 @@
+使用方法：win+R 打开cmd 
+cd至图片目录下 将skeleton.exe 拖入cmd <空格> 图片名
